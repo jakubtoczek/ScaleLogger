757c311bdcef7f2da5bc1b779ace25909f4b35bf
Jakub Toczek <jakub.toczek@inserm.fr>
2026-04-02 13:55:09 +0200

    Fix MSVC C2712 by splitting SEH boundary from C++ unwinding

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -13,14 +13,24 @@ int ShowFatalMessage(const std::string& details) {
 }
 
 #if defined(_MSC_VER)
-int RunMainDialogWithSehGuard(HINSTANCE hInstance, int nCmdShow) {
+int RunMainDialogWithSehBoundary(HINSTANCE hInstance, int nCmdShow, unsigned long* exceptionCodeOut) {
+  if (exceptionCodeOut) *exceptionCodeOut = 0;
   __try {
     return scalelogger::RunMainDialogFresh(hInstance, nCmdShow);
   } __except (EXCEPTION_EXECUTE_HANDLER) {
-    std::ostringstream oss;
-    oss << "Fatal Windows exception (SEH), code=0x" << std::hex << static_cast<unsigned long>(GetExceptionCode());
-    return ShowFatalMessage(oss.str());
+    if (exceptionCodeOut) *exceptionCodeOut = static_cast<unsigned long>(GetExceptionCode());
+    return 1;
   }
+}
+
+int RunMainDialogWithSehGuard(HINSTANCE hInstance, int nCmdShow) {
+  unsigned long exceptionCode = 0;
+  const int code = RunMainDialogWithSehBoundary(hInstance, nCmdShow, &exceptionCode);
+  if (exceptionCode == 0) return code;
+
+  std::ostringstream oss;
+  oss << "Fatal Windows exception (SEH), code=0x" << std::hex << exceptionCode;
+  return ShowFatalMessage(oss.str());
 }
 #else
 int RunMainDialogWithSehGuard(HINSTANCE hInstance, int nCmdShow) {
